fee1679539fc87e5ab83306d0206c230be56f9f3
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-06-02 12:27:40 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
-import Users from './users/pages/Users'
+import { Route, Redirect, Switch } from 'react-router-dom';
+import Users from './users/pages/Users';
+import NewPlace from './places/pages/NewPlace';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Route path="/" exact component={Users} />
+      <Switch>
+        <Route path="/" exact component={Users} />
+        <Route path='/places/new' exact component={NewPlace} />
+        <Redirect to="/" />
+      </Switch>
     </>
   );
 }
---
+++ frontend/src/users/components/UserLIst/UsersList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UsersList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UsersList
