77e291d31a4c6d4f90cfdb89cb365144da7fd29f
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-06-02 17:12:49 +0530

    changes

--- frontend/src/users/components/UserItem/UserItem.js
+++ frontend/src/users/components/UserItem/UserItem.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Avatar from '../../../shared/components/UIElements/Avatar';
+import Card from '../../../shared/components/UIElements/Card'
 import './UserItem.css';
 
 const UserItem = ({ key, id, image, name, placeCount }) => {
   return (
     <li className="user-item">
       <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={image} alt={name} />
-        </div>
+        <Card>
+          <Link to={`/${id}/places`}>
+            <div className="user-item__image">
+              <Avatar image={image} alt={name} />
+            </div>
 
-        <div className="user-item__info">
-          <h2>{name}</h2>
-          <h3>{placeCount} {placeCount === 1 ? ' Place' : ' Places'}</h3>
-        </div>
+            <div className="user-item__info">
+              <h2>{name}</h2>
+              <h3>{placeCount} {placeCount === 1 ? ' Place' : ' Places'}</h3>
+            </div>
+          </Link>
+        </Card>
       </div>
     </li>
   )
