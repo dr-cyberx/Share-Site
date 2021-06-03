913ef38886bb83d6618352266784a161d2954a32
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-06-03 15:32:24 +0530

    changes

--- frontend/src/places/components/PlaceList/PlaceList.js
+++ frontend/src/places/components/PlaceList/PlaceList.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React from 'react';
+import Card from '../../../shared/components/UIElements/Card';
+import PlaceItem from '../PlaceItem/PlaceItem';
 
-const PlaceList = () => {
+
+const PlaceList = ({ items }) => {
+  if (items.length === 0) {
+    return (
+      <div className="place-list center">
+        <Card>
+          <h2>No places found. May be create one ?</h2>
+          <button>Share Place</button>
+        </Card>
+      </div>
+    );
+
+  }
+
   return (
-    <div>
-      
-    </div>
+    <ul className="place-list">
+      {
+        items.map(place => (
+          <PlaceItem
+            key={place.id}
+            id={place.id}
+            image={place.imageUrl}
+            title={place.title}
+            description={place.description}
+            address={place.address}
+            createrId={place.creator}
+            coordinates={place.location}
+          />
+        ))
+      }
+    </ul>
   )
 }
 
