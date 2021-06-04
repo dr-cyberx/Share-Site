b7c1b8aa08584977e845124741f2a564d13af14c
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-06-04 11:05:59 +0530

    added custom Btn

--- frontend/src/places/pages/UserPlace.js
+++ frontend/src/places/pages/UserPlace.js
@@ -1,36 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
 import PlaceList from '../components/PlaceList/PlaceList'
 
 const DUMMY_PLACES = [
   {
     id: 'p1',
     title: 'Empire state Building',
-    description : 'One of the most famous sky scraper in the world',
-    imageUrl : 'https://static.toiimg.com/thumb/71579199.cms?resizemode=75&width=1200&height=900',
-    address : '20 W 34th St, New York, NY 10001, United States',
-    location : {
-      lat : '40.7484445',
-      lng : '-73.9878531'
+    description: 'One of the most famous sky scraper in the world',
+    imageUrl: 'https://static.toiimg.com/thumb/71579199.cms?resizemode=75&width=1200&height=900',
+    address: '20 W 34th St, New York, NY 10001, United States',
+    location: {
+      lat: '40.7484445',
+      lng: '-73.9878531'
     },
-    createrId : 'u1'
+    createrId: 'u1'
   },
   {
     id: 'p1',
     title: 'Empire state Building',
-    description : 'One of the most famous sky scraper in the world',
-    imageUrl : 'https://static.toiimg.com/thumb/71579199.cms?resizemode=75&width=1200&height=900',
-    address : '20 W 34th St, New York, NY 10001, United States',
-    location : {
-      lat : '40.7484445',
-      lng : '-73.9878531'
+    description: 'One of the most famous sky scraper in the world',
+    imageUrl: 'https://static.toiimg.com/thumb/71579199.cms?resizemode=75&width=1200&height=900',
+    address: '20 W 34th St, New York, NY 10001, United States',
+    location: {
+      lat: '40.7484445',
+      lng: '-73.9878531'
     },
-    createrId : 'u2'
+    createrId: 'u2'
   },
 ]
 
 const UserPlace = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.createrId === userId);
   return (
-    <PlaceList items={DUMMY_PLACES}/>
+    <PlaceList items={loadedPlaces} />
   )
 }
 
