97ff6b489ae3d6f68bc8a7ace5df9e737413c6a1
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-06-08 13:35:14 +0530

    changes

--- frontend/src/shared/components/FormComponents/Input.js
+++ frontend/src/shared/components/FormComponents/Input.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import './Input.css';
 
-const Input = () => {
+const inputReducer = (state, actions) => {
+  switch (actions.type) {
+    case 'CHANGES':
+      return {
+        ...state,
+        value: actions.value,
+        isValid: true
+      }
+    default:
+      return state;
+  }
+}
+
+const Input = (props) => {
+  const [inputState, dispatch] = useReducer(inputReducer, { value: '', isValid: false })
+
+  const changeHandler = (event) => {
+    dispatch({
+      type: 'CHANGES',
+      val: event.target.value
+    })
+  }
+
+  const element = props.element === 'input' ?
+    <input
+      id={props.id}
+      type={props.type}
+      placeholder={props.placeholder}
+      onChange={changeHandler}
+      value={inputState.value}
+    /> :
+    <textarea
+      id={props.id}
+      rows={props.rows || 3}
+      onChange={changeHandler}
+      value={inputState.value}
+    />
+
   return (
-    <div>
-      
+    <div className={`form-control`}>
+      <label htmlFor={props.id}>{props.label}</label>
+      {element}
     </div>
   )
 }
