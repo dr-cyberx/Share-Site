a6025b72edfaaec3d478330855e497123595ad90
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-06-08 11:03:54 +0530

    added the modal functionality semi-#1

--- frontend/src/places/components/PlaceItem/PlaceItem.js
+++ frontend/src/places/components/PlaceItem/PlaceItem.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from '../../../shared/components/FormComponents/Button'
 import './PlaceItem.css';
 import Card from '../../../shared/components/UIElements/Card';
+import Modal from '../../../shared/components/UIElements/Modal';
 
 const PlaceItem = ({ image, title, address, description, id }) => {
+  const [showMap, setShowMap] = useState(false);
+
+  const openMaphandler = () => setShowMap(true);
+
+  const closeMaphandler = () => setShowMap(false);
+
   return (
+    <>
+    <Modal 
+      show={showMap} 
+      onCancel={closeMaphandler}
+      header={address} 
+      contentClass="place-item__modal-content"
+      footerClass="place-item__modal-actions"
+      footer={<Button onClick={closeMaphandler}>CLOSE</Button>}
+    >
+      <div className="map-container">
+        <h2>The map! </h2>
+      </div>
+      </Modal>
     <li className="place-item">
       <Card className="place-item__content">
         <div className="place-item__image">
@@ -16,12 +36,13 @@ const PlaceItem = ({ image, title, address, description, id }) => {
           <p>{description}</p>
         </div>
         <div className="place-item__actions">
-          <Button inverse>VIEW ON MAP</Button>
+          <Button inverse onClick={openMaphandler}>VIEW ON MAP</Button>
           <Button to={`/places/${id}`}>EDIT</Button>
           <Button danger>DELETE</Button>
         </div>
       </Card>
     </li>
+    </>
   )
 }
 
--- frontend/src/shared/components/UIElements/Modal.js
+++ frontend/src/shared/components/UIElements/Modal.js
@@ -1,9 +1,11 @@
-import React, { Children } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import {CSSTransition} from 'react-transition-group';
 
+import Backdrop from './Backdrop';
 import './Modal.css';
 
-const ModalOverlay = ({ className, style, headerClass, header, onSubmit, contentClass, children, footerClass }) => {
+const ModalOverlay = ({ className, style, headerClass, header, onSubmit, contentClass, children, footerClass, footer }) => {
   const content = (
     <div className={`modal ${className}`} style={style}>
       <header className={`modal__header ${headerClass}`}>
@@ -14,7 +16,7 @@ const ModalOverlay = ({ className, style, headerClass, header, onSubmit, content
           {children}
         </div>
         <footer className={`modal__footer ${footerClass}`}>
-          
+          {footer}
         </footer>
       </form>
     </div>
@@ -22,11 +24,20 @@ const ModalOverlay = ({ className, style, headerClass, header, onSubmit, content
   return ReactDOM.createPortal(content, document.getElementById('modal-hook'))
 }
 
-const Modal = () => {
+const Modal = (props) => {
   return (
-    <div>
-
-    </div>
+    <>
+      {props.show && <Backdrop  onClick={props.onCancel} />}
+      <CSSTransition 
+        in={props.show}
+        mountOnEnter
+        unmountOnExit
+        timeout={200}
+        classNames="modal"
+      >
+      <ModalOverlay {...props} />
+      </CSSTransition>
+    </>
   )
 }
 
