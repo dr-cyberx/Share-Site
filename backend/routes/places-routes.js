a69e5bdb7ffc79e1de6e845c6573e13345f06ab5
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-06-25 16:27:26 +0530

    added the dyamic routes of the sub routes files

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,9 +2,37 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  console.log("Get requests in Places!");
-  res.json({ message: 'All works fine' })
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire state Building',
+    description: 'One of the most famous sky scraper in the world',
+    imageUrl: 'https://static.toiimg.com/thumb/71579199.cms?resizemode=75&width=1200&height=900',
+    address: '20 W 34th St, New York, NY 10001, United States',
+    location: {
+      lat: '40.7484445',
+      lng: '-73.9878531'
+    },
+    createrId: 'u1'
+  },
+  {
+    id: 'p2',
+    title: 'Emp. state Building',
+    description: 'One of the most famous sky scraper in the world',
+    imageUrl: 'https://static.toiimg.com/thumb/71579199.cms?resizemode=75&width=1200&height=900',
+    address: '20 W 34th St, New York, NY 10001, United States',
+    location: {
+      lat: '40.7484445',
+      lng: '-73.9878531'
+    },
+    createrId: 'u2'
+  },
+]
+
+router.get('/:pid', (req, res) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find(p=> p.id === placeId)
+  res.json({place})
 })
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const placesRoutes = require('./routes/places-routes');
 
 const hostname = '127.0.0.1';
 const port = 4000;
 
 const app = express();
 
+app.use('/api/places', placesRoutes);
 
 app.listen(port, hostname, ()=>{
   console.log(`The backend Server is running at http://${hostname}:${port}`)
