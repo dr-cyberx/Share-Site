85c4fe5d34b3deaae3d959d846716a32ad367665
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-06-28 13:47:26 +0530

    wrap up the backend

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -99,6 +99,9 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
+  if(!DUMMY_PLACES.find(p => p.id === placeId)){
+    throw new HttpErrors('no places are found with this id ', 404)
+  }
   DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
   res.status(200).json({ message: 'Deleted successfully!' });
 };
