b87cfabbdb1d73bdf06e3e913663f3bef3eb0edb
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-07-20 18:29:01 +0530

    added user schema and login api setup with sign up

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,36 +1,37 @@
-const { validationResult } = require('express-validator');
-const { v4: uuidv4 } = require('uuid');
+const { validationResult } = require("express-validator");
+const { v4: uuidv4 } = require("uuid");
 
-const Place = require('../models/place');
-const HttpErrors = require('../models/Http-errors');
-
+const Place = require("../models/place");
+const HttpErrors = require("../models/Http-errors");
 
 let DUMMY_PLACES = [
   {
-    id: 'p1',
-    title: 'Empire state Building',
-    description: 'One of the most famous sky scraper in the world',
-    imageUrl: 'https://static.toiimg.com/thumb/71579199.cms?resizemode=75&width=1200&height=900',
-    address: '20 W 34th St, New York, NY 10001, United States',
+    id: "p1",
+    title: "Empire state Building",
+    description: "One of the most famous sky scraper in the world",
+    imageUrl:
+      "https://static.toiimg.com/thumb/71579199.cms?resizemode=75&width=1200&height=900",
+    address: "20 W 34th St, New York, NY 10001, United States",
     location: {
-      lat: '40.7484445',
-      lng: '-73.9878531'
+      lat: "40.7484445",
+      lng: "-73.9878531",
     },
-    creatorId: 'u1'
+    creatorId: "u1",
   },
   {
-    id: 'p2',
-    title: 'Emp. state Building',
-    description: 'One of the most famous sky scraper in the world',
-    imageUrl: 'https://static.toiimg.com/thumb/71579199.cms?resizemode=75&width=1200&height=900',
-    address: '20 W 34th St, New York, NY 10001, United States',
+    id: "p2",
+    title: "Emp. state Building",
+    description: "One of the most famous sky scraper in the world",
+    imageUrl:
+      "https://static.toiimg.com/thumb/71579199.cms?resizemode=75&width=1200&height=900",
+    address: "20 W 34th St, New York, NY 10001, United States",
     location: {
-      lat: '40.7484445',
-      lng: '-73.9878531'
+      lat: "40.7484445",
+      lng: "-73.9878531",
     },
-    creatorId: 'u2'
+    creatorId: "u2",
   },
-]
+];
 
 const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
@@ -39,7 +40,10 @@ const getPlaceById = async (req, res, next) => {
   try {
     place = await Place.findById(placeId);
   } catch (err) {
-    const error = new HttpErrors('something went wrong , could not find place by id', 500);
+    const error = new HttpErrors(
+      "something went wrong , could not find place by id",
+      500
+    );
     return next(error);
   }
 
@@ -47,24 +51,34 @@ const getPlaceById = async (req, res, next) => {
     // const error = new Error('Could not find place of the provided place id!');
     // error.code = 404;
     // throw error;
-    const error = new HttpErrors('could not find place of the provided place id!');
+    const error = new HttpErrors(
+      "could not find place of the provided place id!",
+      500
+    );
     return next(error);
   }
-  res.json({ place: place.toObject({ getters: true }) })
-}
+  res.json({ place: place.toObject({ getters: true }) });
+};
 
-
-const getPlacesByUserId = (req, res, next) => {
+const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  const places = DUMMY_PLACES.filter(p => p.creatorId === userId);
+
+  let places;
+  try {
+    places = await Place.find({ creatorId: userId });
+  } catch (err) {
+    const error = new HttpErrors(
+      "could not find place with this user id ",
+      500
+    );
+    return next(error);
+  }
 
   if (!places || places.length === 0) {
-    return next(
-      new HttpErrors('could not find place of this user id ', 404)
-    )
+    return next(new HttpErrors("could not find place of this user id ", 404));
   }
-  res.json({ places });
-}
+  res.json({ places: places.toObject({ getters: true }) });
+};
 
 const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
@@ -76,49 +90,69 @@ const createPlace = async (req, res, next) => {
     title,
     description,
     location: coordinates,
-    image: 'https://images.unsplash.com/photo-1555109307-f7d9da25c244?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZW1waXJlJTIwc3RhdGUlMjBidWlsZGluZ3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+    image:
+      "https://images.unsplash.com/photo-1555109307-f7d9da25c244?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZW1waXJlJTIwc3RhdGUlMjBidWlsZGluZ3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
     address,
-    creatorId
-  })
+    creatorId,
+  });
 
   try {
     await createdPlace.save();
   } catch (err) {
-    const error = new HttpErrors(
-      'Creating place failed',
-      500
-    )
+    const error = new HttpErrors("Creating place failed", 500);
     return next(error);
   }
 
   res.status(201).json({ place: createdPlace });
-}
+};
 
 const updatePlace = (req, res, next) => {
   const inputErrors = validationResult(req);
   if (!inputErrors.isEmpty()) {
-    throw new HttpErrors(`${inputErrors.errors[0].param} should not be empty `, 422)
+    throw new HttpErrors(
+      `${inputErrors.errors[0].param} should not be empty `,
+      422
+    );
   }
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
-  const updatedplace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
-  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+  const updatedplace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
   updatedplace.title = title;
   updatedplace.description = description;
 
   DUMMY_PLACES[placeIndex] = updatedplace;
 
-  res.status(200).json({ place: updatedplace })
+  res.status(200).json({ place: updatedplace });
 };
 
-const deletePlace = (req, res, next) => {
+const deletePlace = async (req, res, next) => {
   const placeId = req.params.pid;
-  if (!DUMMY_PLACES.find(p => p.id === placeId)) {
-    throw new HttpErrors('no places are found with this id ', 404)
+
+  let place;
+
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpErrors(
+      "something went wrong , can't delete place",
+      500
+    );
+    return next(error);
   }
-  DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
-  res.status(200).json({ message: 'Deleted successfully!' });
+
+  try {
+     await place.remove();
+  } catch (err) {
+    const error = new HttpErrors(
+      "something went wrong , can't delete place",
+      500
+    );
+    return next(error);
+  }
+
+  res.status(200).json({ message: "Deleted successfully!" });
 };
 
 exports.getPlaceById = getPlaceById;
---
+++ backend/models/users.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    required: true,
+    type: String,
+  },
+  email: {
+    required: true,
+    type: String,
+    unique: true,
+  },
+  password: {
+    required: true,
+    type: String,
+    minlength: 6,
+  },
+  image: {
+    required: true,
+    type: String,
+  },
+  places: {
+    required: true,
+    type: String,
+  },
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema);
